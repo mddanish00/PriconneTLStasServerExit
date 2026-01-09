8901873bb0c6b729d4794ab7136c1cd95b46720d
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2026-01-10 00:10:17 +0800

    feat: add StasServerExit plugin implementation
    Introduce the StasServerExit BepInEx plugin to automatically close StasServer when Priconne exits. Includes a logging utility (Log.cs) and the main plugin logic (Plugin.cs) with Harmony patches to track and terminate the StasServer process on application quit.

---
+++ StasServerExit/Log.cs
@@ -0,0 +1,116 @@
+﻿/*
+    PriconneTLStasServerExit is a BepInEx plugin to automatically close StasServer when closing Priconne.
+    Copyright (C) 2026  mddanishh00
+    Copyright (C) 2023  Kevga
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    This plugin is based on SugoiExitPatch from PriconneTLFixup (https://github.com/Kevga/PriconneTLFixup).
+*/
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using BepInEx.Logging;
+
+namespace StasServerExit;
+
+public class Log
+{
+    internal static ManualLogSource? BieLogger;
+
+#if DEBUG
+    private static void LogFactory(string message, LogLevel logLevel, string filePath, string member, int line)
+    {
+        var pathParts = filePath.Split('\\');
+        var className = pathParts[^1].Replace(".cs", "");
+        var caller = new StackFrame(3, true).GetMethod()?.Name;
+        var prefix = $"[{caller}->{className}.{member}:{line}]: ";
+        BieLogger?.Log(logLevel, $"{prefix}{message}");
+    }
+
+    public static void Debug(string message, [CallerFilePath] string filePath = "",
+        [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
+    {
+        LogFactory(message, LogLevel.Debug, filePath, member, line);
+    }
+
+    public static void Info(string message, [CallerFilePath] string filePath = "",
+        [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
+    {
+        LogFactory(message, LogLevel.Info, filePath, member, line);
+    }
+
+    public static void Warn(string message, [CallerFilePath] string filePath = "",
+        [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
+    {
+        LogFactory(message, LogLevel.Warning, filePath, member, line);
+    }
+
+    public static void Error(string message, [CallerFilePath] string filePath = "",
+        [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
+    {
+        LogFactory(message, LogLevel.Error, filePath, member, line);
+    }
+#else
+    private static void LogFactory(string message, LogLevel logLevel)
+    {
+        BieLogger?.Log(logLevel, message);
+    }
+
+    public static void Debug(string message, bool evenInReleaseBuild)
+    {
+        LogFactory(message, LogLevel.Debug);
+    }
+
+    public static void Info(string message)
+    {
+        LogFactory(message, LogLevel.Info);
+    }
+
+    public static void Warn(string message)
+    {
+        LogFactory(message, LogLevel.Warning);
+    }
+
+    public static void Error(string message)
+    {
+        LogFactory(message, LogLevel.Error);
+    }
+#endif
+    [Conditional("DEBUG")]
+    public static void Debug(Exception exception)
+    {
+        BieLogger?.Log(LogLevel.Debug, exception);
+    }
+
+    [Conditional("DEBUG")]
+    public static void Debug(string message)
+    {
+        BieLogger?.Log(LogLevel.Debug, message);
+    }
+
+    public static void Info(Exception exception)
+    {
+        BieLogger?.Log(LogLevel.Info, exception);
+    }
+
+    public static void Warn(Exception exception)
+    {
+        BieLogger?.Log(LogLevel.Warning, exception);
+    }
+
+    public static void Error(Exception exception)
+    {
+        BieLogger?.Log(LogLevel.Error, exception);
+    }
+}
---
+++ StasServerExit/Plugin.cs
@@ -0,0 +1,89 @@
+﻿/*
+    PriconneTLStasServerExit is a BepInEx plugin to automatically close StasServer when closing Priconne.
+    Copyright (C) 2026  mddanishh00
+    Copyright (C) 2023  Kevga
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    This plugin is based on SugoiExitPatch from PriconneTLFixup (https://github.com/Kevga/PriconneTLFixup).
+*/
+using System.Diagnostics;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using Cute;
+using HarmonyLib;
+using StasServer;
+
+namespace StasServerExit;
+
+
+[BepInPlugin("StasServerExit", "PriconneTLStasServerExit by mddanish00", "1.0.0")]
+[BepInProcess("PrincessConnectReDive.exe")]
+public class Plugin : BasePlugin
+{
+    private readonly Harmony harmony = new("com.github.mddanish00.PriconneTLStasServerExit");
+
+    public override void Load()
+    {
+        StasServerExit.Log.BieLogger = Log;
+
+        try
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+        catch (Exception e)
+        {
+            StasServerExit.Log.Warn("Failed to set console encoding to UTF8. Japanese characters may not display correctly.");
+            StasServerExit.Log.Warn(e);
+        }
+
+        var sw = Stopwatch.StartNew();
+        harmony.PatchAll();
+        sw.Stop();
+        StasServerExit.Log.Debug($"Patching took {sw.ElapsedMilliseconds}ms");
+        StasServerExit.Log.Info("StasServerExit loaded!");
+    }
+
+    public override bool Unload()
+    {
+        StasServerExit.Log.Info("Shutting down...");
+        harmony.UnpatchSelf();
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(StasServerEndpoint), "StartProcess")]
+public class StasServerExitPatch
+{
+    internal static Process? process;
+
+    public static void Postfix(Process ___process)
+    {
+        Log.Info("StasServerEndpoint.StartProcess");
+        process = ___process;
+    }
+}
+
+[HarmonyPatch(typeof(Toolbox), nameof(Toolbox.ApplicationQuit))]
+public class StasServerExitPatch2
+{
+    public static void Prefix()
+    {
+        Log.Info("Application quitting");
+        StasServerExitPatch.process?.Kill();
+        StasServerExitPatch.process = null;
+    }
+}
+
+
